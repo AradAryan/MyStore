6dddcd0a1876f4def7666987de19da2a27491aec
Arad <aradaryanbusiness@gmail.com>
2021-11-03 19:58:12 +0330

    Making Some Changes

--- MyStore/Controllers/Base/BaseController.cs
+++ MyStore/Controllers/Base/BaseController.cs
@@ -6,7 +6,6 @@ namespace MyStore.Controllers
 {
     public class BaseController : Controller
     {
-        //protected StoreDbContext Context => (StoreDbContext)HttpContext.RequestServices.GetService(typeof(StoreDbContext));
         protected ILogger<Controller> Logger => (ILogger<Controller>)HttpContext.RequestServices.GetService(typeof(ILogger<Controller>));
 
     }
